93bbec9d09be9ab466774a53f1f643ceb7302c53
pyephebes0 <xyz928.pyephebes@gmail.com>
2025-06-21 22:11:42 +0700

    Update +page.svelte

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,6 +1,5 @@
 <script>
   import Setting from '$lib/components/Setting.svelte';
-  import UsersIcon from '$lib/components/UsersIcon.svelte';
 
   function addUser() {
     // ฟังก์ชันสำหรับเพิ่มผู้ใช้ใหม่
