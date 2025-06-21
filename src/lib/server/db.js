e4c305b582956c7f4bc34cc071c4af29865d1727
pyephebes0 <xyz928.pyephebes@gmail.com>
2025-06-21 18:32:45 +0700

    db js
    update db

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -21,7 +21,8 @@ const userSchema = new mongoose.Schema({
   email:    { type: String, required: true, unique: true },
   password: { type: String, required: true }
 }, {
-  timestamps: true // ✅ สำคัญมาก
+  timestamps: true, // ✅ สำคัญมาก
+  versionKey: false  // ✅ ปิดไม่ให้สร้าง __v
 });
 
 // ✅ ใช้ชื่อ model ว่า 'User' ไม่ควรใช้ 'Users'
